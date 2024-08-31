928d211462db29fa8ef24e4c8e10caf0dd49fa98
PythonPuzzles <Pythonpuzzles@gmail.com>
2024-08-31 14:37:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def example_b():
     print('\nExample B')
     print('~~~~~~~~~')
 
-    # Unicode colors
+    # Unicode / ANSI colors
     red = '\033[31m'
     blue = '\033[34m'
     green = '\033[32m'
